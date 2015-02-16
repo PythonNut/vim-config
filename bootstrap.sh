cc4bbe1da314c6179284dd3c8ff9400206fba19a
PythonNut <git config --global user.email>
2015-02-16 13:23:58 -0800

    Prefer gvim in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,10 @@ mkdir -p ~/.vim/bundle
 cd ~/.vim
 git clone https://github.com/Shougo/neobundle.vim bundle/neobundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc 2> /dev/null
-vim
+
+if [ -f /usr/bin/gvim ]; then
+  gvim -v
+else
+  vim
+fi
 
