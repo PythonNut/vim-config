22012d5529d397efe1ec85903b87b80d23009a7f
PythonNut <PythonNut@users.noreply.github.com>
2015-05-05 21:48:11 +0000

    Allow vim to be bootstrapped non-interactively

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,3 @@ mkdir -p ~/.vim/bundle
 cd ~/.vim
 git clone https://github.com/Shougo/neobundle.vim bundle/neobundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc 2> /dev/null
-
-if [ -f /usr/bin/gvim ]; then
-  gvim -v
-else
-  vim
-fi
-
